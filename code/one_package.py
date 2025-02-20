515947fe605e96774a1f9e2c6a23a6958074aec4
ccsirey <ccsirey@syr.edu>
2025-02-20 13:36:55 -0500

    first commit

--- code/one_package.py
+++ code/one_package.py
@@ -6,3 +6,19 @@ Calculate the total package size and display that.
 
 see one_package.png for a screenshot
 '''
+import streamlit as st
+import packaging
+
+package = st.text_input('Enter package conversions split by "/":')
+enter = st.button("get total")
+
+if enter:
+    parsed_package = packaging.parse_packaging(package)
+    total_units = packaging.calc_total_units(parsed_package)
+    unit = packaging.get_unit(parsed_package)
+    st.text(parsed_package)
+    for item in parsed_package:
+        name = list(item.keys())[0]
+        size = list(item.values())[0]
+        st.info(f"{name} :arrow_right: {size}")
+    st.success(f"total :package: size  {total_units} {unit}")
